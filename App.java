4a42e97f869c9f62169f6b7c77eff8d943807111
cOUCOU1232 <ranaiv.hasimbola0@gmail.com>
2026-04-09 22:07:42 +0200

    petit exemple simple, gestion des tours, affichages des coups joués, prototype de l'affichage de l'echequier

--- App.java
+++ App.java
@@ -1,11 +1,44 @@
+import java.util.ArrayList;
+
 public class App {
   String name;
+  Boolean end = false;
+  Boolean turn = true; // True: joueur 1, False: joueur 2
+  ArrayList<Integer> moves = new ArrayList<>();
 
   App(String name) {
     this.name = name;
   }
 
   public void launch() {
-    IO.println("Hello");
+    while (!end) {
+      showGame();
+      showMoves();
+      play();
+    }
+  }
+
+  public void showGame() {
+    IO.println(this.name + ": ");
+    IO.println("<Affichage du tableau>");
+  }
+
+  public void showMoves() {
+    IO.println("Moves: " + this.moves.toString());
+  }
+
+  public void play() {
+    Integer player;
+    if (this.turn)
+      player = 1;
+    else
+      player = 2;
+
+    // exemple d'input
+    Integer cell = Integer.valueOf(IO.readln("Joueur " + player + ": "));
+    // exemple d'output
+    this.moves.add(cell);
+    this.turn = !this.turn;
+    // this.end = true;
   }
 }
