974fb2f344c974f09e075cc8677d45a421392aba
Denis <denissoso07@gmail.com>
2026-04-16 15:05:05 +0200

    deuxieme

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     void main() {
         App app = new App("Havannah", 5);
         app.launch();
-        // modification test denis dans le main
+        // deuxieme tesy de sync 
     }
 }
