7aeec5f83049329c1418e78e158ecb2397a2e471
Denis <denissoso07@gmail.com>
2026-05-03 23:35:39 +0200

    ajout d'une bibliothèque graphique

---
+++ graphics/echequier.java
@@ -0,0 +1,9 @@
+public class echequier {
+    public static void main(String[] args) {
+         Rectangle box = new Rectangle(5, 10, 60, 80);
+  
+        box.translate(20, 30);
+        box.draw();
+    }
+   
+}
