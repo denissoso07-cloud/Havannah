59e7c4d6fb6871bcfdcad1255382fe0cbace144f
Denis <denissoso07@gmail.com>
2026-05-04 14:02:12 +0200

    Ajout du plateau dans la class Main et des fonctions pour tester le graphique des pions joueur1 et 2.

--- Main.java
+++ Main.java
@@ -9,6 +9,22 @@ public class Main {
     void main() {
         App app = new App("Havannah", 5);
         app.launch();
+        
+        // plateau
+        echequier jeu = new echequier();
+        jeu.echec();
+
+        // test  : pion J1 sur la case j=0, i=0
+        double[] c1 = jeu.getCentre(0, 0);
+        jeu.PionJ1(c1[0], c1[1]);
+
+        // test  : pion J2 sur la case j=2, i=3
+        double[] c2 = jeu.getCentre(2, 3);
+        jeu.PionJ2(c2[0], c2[1]);
+
+        // test : gem
+        jeu.gem();
+
 
         // test click cell
         Cell test = new Cell(4, 3);
--- graphics/echequier.java
+++ graphics/echequier.java
@@ -1,13 +1,12 @@
 
 public class echequier {
-
-   public static void echec() {
     double radius = 20;
     double h = 1.7 * radius;    // hauteur
     double w = 30;              // espacement vertical entre centres
     double startX = 450;         // position horizontale 
     double startY = 170;      // point de départ en Y
-    double maxCols = 15;    
+    double maxCols = 15;  
+   public void echec() {  
 
     for(int j = 0;j< maxCols;j++){
         double cx = startX + j * w;
@@ -33,53 +32,44 @@ public class echequier {
     }
 }
 }
- public static void PionJ1(double cx, double cy, double radius) {
+ public void PionJ1(double cx, double cy) {
     // cx, cy = centre de l'hexagone
     // on décale de -radius pour avoir le coin haut-gauche de l'ellipse
-    Ellipse j1 = new Ellipse(cx - radius, cy - radius, radius * 2, radius * 2);
+    Ellipse j1 = new Ellipse(cx - radius, cy - radius,30, 30);
     j1.setColor(Color.BLACK);
     j1.fill();
    
 }
 
-public static void PionJ2(double cx, double cy, double radius) {
-    Ellipse j2 = new Ellipse(cx - radius, cy - radius, radius * 2, radius * 2);
+public void PionJ2(double cx, double cy) {
+    Ellipse j2 = new Ellipse(cx - radius, cy - radius,30, 30);
     j2.setColor(Color.GRAY);
     j2.fill();
    
 }
 
-  public static void gem(){
+  public void gem(){
         Hexagone gems = new Hexagone(45, 45, 10);
         gems.setColor(Color.GREEN);
         gems.fill();
 
     }
-public static double[] getCentre(int j, int i) {
-    double radius = 20;
-    double h = 1.7 * radius;
-    double w = 30;
-    double startX = 450;
-    double startY = 170;
-    double maxCols = 15;
-
-    double cx = startX + j * w;
+    
+public double[] getCentre(int j, int i) { 
+    double cx = startX + j * w +5;
     double offsetY;
     if (j < maxCols / 2) {
         offsetY = -(j * h / 2);
     } else {
         offsetY = -((maxCols - 1 - j) * h / 2);
     }
-    double cy = startY + offsetY + i * h;
-
+    double cy = startY + offsetY + i * h ;
+    cy += 5;
     return new double[]{cx, cy};
 }
 
 public static void main(String[] args) {
-    double[] centre = getCentre(1, 1);
   
-    PionJ1(centre[0], centre[1], 20);
-    echec();
 }
 }
 
